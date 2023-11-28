05205eed38e8a98099752de5bee49d0a999e483b
Ya-shu1 <103036816+Ya-shu1@users.noreply.github.com>
2023-11-28 09:51:58 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,3 @@
 https://github.com/Ya-shu1/maven-web-app.git
-cd /home/ec2-user/workspace/java2/maven-web-app/target
+cd /home/ec2-user/workspace/java bash/maven-web-app/target
 sudo cp -r 01-maven-web-app.war /home/ec2-user/workspace/tomcat/apache-tomcat-8.5.93/webapps
